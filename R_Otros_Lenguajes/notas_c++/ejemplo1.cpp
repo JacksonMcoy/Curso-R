a4593c7434b3eb4c6a531a4020e1b47a21238762
C-Lara <claraa@uni.edu.pe>
2016-04-28 01:04:47 -0500

    Ejemplo1: hola mundo en c++

---
+++ R_Otros_Lenguajes/notas_c++/ejemplo1.cpp
@@ -0,0 +1,13 @@
+// Hola en c++
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+        string s;
+        s ="Hola R, soy C++";
+        cout << s << endl;
+        return 0;
+}
