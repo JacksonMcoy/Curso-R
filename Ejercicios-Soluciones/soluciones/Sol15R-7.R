3f4f7e841c8c7f382e86a6b84835f7d82a4647a3
C-Lara <claraa@uni.edu.pe>
2016-06-02 13:13:25 -0500

    Solucion15 de EjercicioR-7

---
+++ Ejercicios-Soluciones/soluciones/Sol15R-7.R
@@ -0,0 +1,7 @@
+# Una funcion es vectorizada si devuelve un valor para 
+# argumentos vectoriales usando las reglas del
+# recycling
+
+f <- function(x){
+  ifelse(x > 0, sqrt(x) + sin(y), y + cos(x))
+}
