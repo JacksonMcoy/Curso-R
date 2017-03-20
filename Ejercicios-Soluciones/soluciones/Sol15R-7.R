b73c557e288e2752bbe7e21b3d919de439fbe495
C-Lara <claraa@uni.edu.pe>
2017-03-20 16:18:22 -0500

    Eliminando directorio

--- Ejercicios-Soluciones/soluciones/Sol15R-7.R
+++
@@ -1,7 +0,0 @@
-# Una funcion es vectorizada si devuelve un valor para 
-# argumentos vectoriales usando las reglas del
-# recycling
-
-f <- function(x){
-  ifelse(x > 0, sqrt(x) + sin(y), y + cos(x))
-}
--- Ejercicios-Soluciones/soluciones/Sol9R-7.R
+++
@@ -1,5 +0,0 @@
-# Solucion 9
-
-apply(x, 1, max)
-apply(x, 1, function(x) mean(x[x>0], na.rm=TRUE))
-apply(x, 1, function(x) x[c(FALSE, x[-1] > x[-length(x)])][1])
